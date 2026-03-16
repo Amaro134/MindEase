ea5195bbef3ef68b57b8d2764d8096d233d4c56e
Amaro134 <amarogracie134@gmail.com>
2026-03-16 16:29:41 +0300

    Enable auto admin creation via UsersConfig

--- moods/users/apps.py
+++ moods/users/apps.py
@@ -1,6 +1,30 @@
 from django.apps import AppConfig
+import os
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError, ProgrammingError
+
+        admin_email = os.environ.get("ADMIN_EMAIL")
+        admin_password = os.environ.get("ADMIN_PASSWORD")
+        admin_name = os.environ.get("ADMIN_FULL_NAME", "Admin")
+
+        if not admin_email or not admin_password:
+            return
+
+        User = get_user_model()
+        try:
+            if not User.objects.filter(is_superuser=True).exists():
+                User.objects.create_superuser(
+                    email=admin_email,
+                    password=admin_password,
+                    full_name=admin_name,
+                )
+        except (OperationalError, ProgrammingError):
+            # Database not ready (e.g., during initial migrations)
+            pass
